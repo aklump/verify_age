fb1d543cff1d1796ee0fa9a0a024d48cbe4da712
aklump <sourcecode@intheloftstudios.com>
2014-01-10 14:21:19 -0800

    Version bumped from 0.1.5 to 0.1.6

--- verify_age/scripts/verify_age.js
+++ verify_age/scripts/verify_age.js
@@ -1,5 +1,5 @@
 /*!
- * Verify Age jQuery JavaScript Plugin v0.1.5
+ * Verify Age jQuery JavaScript Plugin v0.1.6
  * http://www.intheloftstudios.com/packages/php/verify_age
  *
  * A PHP/Javascript solution for SEO/Google-friendly website age-verification.
@@ -7,7 +7,7 @@
  * Copyright 2013, Aaron Klump
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
- * Date: Fri, 10 Jan 2014 12:40:45 -0800
+ * Date: Fri, 10 Jan 2014 14:21:14 -0800
  */
 ;(function($, undefined) {
 "use strict";
@@ -50,7 +50,7 @@ $.fn.verifyAge = function() {
   return this;
 };
 
-$.fn.verifyAge.version = function() { return '0.1.5'; };
+$.fn.verifyAge.version = function() { return '0.1.6'; };
 
 })(jQuery);
 
