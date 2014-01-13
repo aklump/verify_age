b1786a0ca3f3a5529d6151ea5a498b943e31f834
aklump <sourcecode@intheloftstudios.com>
2014-01-13 12:28:15 -0800

    fixed build script

--- js/verify_age.js
+++ js/verify_age.js
@@ -7,7 +7,11 @@
  * Copyright 2013, Aaron Klump
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
+<<<<<<< Updated upstream
  * Date: Mon, 13 Jan 2014 12:27:27 -0800
+=======
+ * Date: Mon, 13 Jan 2014 12:26:15 -0800
+>>>>>>> Stashed changes
  */
 ;(function($, undefined) {
 "use strict";
